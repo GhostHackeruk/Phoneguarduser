df4deabfc420a2daa0aff8159ec5f2dce70da206
Ghost Hacker <ghost.hacker.uk@gmail.com>
2026-01-31 01:29:47 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,11 +6,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyD_eOcX7P77xLFnOPf3yWk3P-Qyhbyab5E",
   authDomain: "phone-guard-user-new.firebaseapp.com",
   projectId: "phone-guard-user-new",
-  storageBucket: "phone-guard-user-new.appspot.com",
+  storageBucket: "phone-guard-user-new.firebasestorage.app", // ✅ fixed
   messagingSenderId: "764033397081",
   appId: "1:764033397081:web:182fd2c7cfc663e36c5c48"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);   // ✅ এটা না থাকলে সব ভাঙে
+export const db = getFirestore(app);
